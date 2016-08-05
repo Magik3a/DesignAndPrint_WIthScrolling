f7244615083b6fcc24a06e19efae869444db42bf
Svetlin Krastanov <s.krastanov@televic.com>
2016-08-05 14:23:09 +0300

    Major bug, commit is only to show someone

--- DesignAndPrint/App_Start/BundleConfig.cs
+++ DesignAndPrint/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace DesignAndPrint
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
