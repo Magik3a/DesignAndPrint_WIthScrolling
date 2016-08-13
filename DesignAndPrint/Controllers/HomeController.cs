512146850d1674050dd1f8027449f43b3513e464
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-08-14 00:26:01 +0300

    Some functionalities and validations

--- DesignAndPrint/Controllers/HomeController.cs
+++ DesignAndPrint/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
 ﻿using DesignAndPrint.Helpers;
 using DesignAndPrint.Models;
+using HtmlAgilityPack;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -96,6 +102,86 @@ namespace DesignAndPrint.Controllers
 
             }
             return PartialView("Rows/Template", template);
+        }
+        
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult DownloadStickers(string html, string pagesize)
+        {
+            if(String.IsNullOrWhiteSpace(pagesize))
+                return Json(false);
+            try
+            {
+                byte[] bytes = GeneratePDF(html, pagesize);
+                
+                // Generate a new unique identifier against which the file can be stored
+                string handle = Guid.NewGuid().ToString();
+                Session[handle] = bytes.ToArray();
+
+                //return File(bytes, "application/pdf", DateTime.Now.ToString(CultureInfo.InvariantCulture));
+
+                // Note we are returning a filename as well as the handle
+                return new JsonResult()
+                {
+                    Data = new { fileGuid = handle }
+                };
+            }
+            catch (Exception ex)
+            {
+                return Json(false);
+            }
+        }
+        [HttpGet]
+        public virtual ActionResult Download(string fileGuid)
+        {
+            if (Session[fileGuid] != null)
+            {
+                byte[] data = Session[fileGuid] as byte[];
+                return File(data, "application/pdf", DateTime.Now.ToString(CultureInfo.InvariantCulture) +".pdf");
+            }
+            else
+            {
+                // Problem - Log the error, generate a blank file,
+                //           redirect to another controller action - whatever fits with your application
+                return new EmptyResult();
+            }
+        }
+        public byte[] GeneratePDF(string html, string pageSize)
+        {
+            #region Generate PDF
+            Byte[] bytes;
+            var ms = new MemoryStream();
+            //Create an iTextSharp Document wich is an abstraction of a PDF but **NOT** a PDF
+            var doc = new Document();
+            if (pageSize == "A4")
+                doc = new Document(PageSize.A4);
+            else
+                doc = new Document(PageSize.LETTER);
+            var writer = PdfWriter.GetInstance(doc, ms);
+            doc.Open();
+            doc.NewPage();
+            var hDocument = new HtmlDocument()
+            {
+                OptionWriteEmptyNodes = true,
+                OptionAutoCloseOnEnd = true
+            };
+            hDocument.LoadHtml(html);
+
+            var closedTags = hDocument.DocumentNode.WriteTo();
+            var example_html = closedTags;
+            var example_css = System.IO.File.ReadAllText(Server.MapPath("~/Content/Site.css"));
+            example_css += System.IO.File.ReadAllText(Server.MapPath("~/Content/Templates.min.css"));
+            var msCss = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(example_css));
+            var msHtml = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(example_html));
+            iTextSharp.tool.xml.XMLWorkerHelper.GetInstance().ParseXHtml(writer, doc, msHtml, msCss, Encoding.UTF8);
+            doc.Close();
+            bytes = ms.ToArray();
+
+            var testFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "test.pdf");
+              System.IO.File.WriteAllBytes(testFile, bytes);
+            #endregion
+
+            return bytes;
         }
         public ActionResult About()
         {
--- DesignAndPrint/Scripts/clickevents.js
+++ DesignAndPrint/Scripts/clickevents.js
@@ -4,7 +4,7 @@
         opacity: 1
     }, 1500);
 });
-
+// Middle buttons for scrolling 
 $('#btnContinueWithPapers').click(function () {
 
     $(this).animate({
@@ -22,13 +22,7 @@ $('#btnContinueWithPapers').click(function () {
 });
 $('#btnContinueWiTemplates').click(function () {
     console.log($(".UsPaper").hasClass('active-paper'));
-    if (!$(".a4size").hasClass('active-paper') && !$(".UsPaper").hasClass('active-paper')) {
-        $("#wrapper").overhang({
-            type: "warn",
-            message: "You have to choose paper size!",
-            duration: 2,
-            upper: true
-        });
+    if (!ValidatePaperBeforeFinnish()) {
         return;
     }
     $(this).animate({
@@ -45,13 +39,7 @@ $('#btnContinueWiTemplates').click(function () {
     });
 });
 $('#btnContinueWithPictures').click(function () {
-    if (!$(".template").hasClass('active-template')) {
-        $("#wrapper").overhang({
-            type: "warn",
-            message: "You have to choose template!",
-            duration: 2,
-            upper: true
-        });
+    if (!ValidateTemplateBeforeFinnish()) {
         return;
     }
     $(this).animate({
@@ -78,12 +66,119 @@ $('#btnFinishAndPrint').click(function () {
     }, 500);
 });
 
+// Last buttons for printing and downloading
 $('#print-button').click(function () {
     $('.btn-add-image').hide();
     $(".box").removeClass("active-box");
-    $("#page-for-printing").printElement({ printMode: 'popup' });
+    $("#page-for-printing").printElement({
+        printMode: 'popup',
+        importStyle: false,
+        printContainer: false,
+        debug: true
+    });
+});
+$("#download-pdf-button").click(function () {
+    var param = {
+        html: $("#page-for-printing").html()
+    };
+
+    //$.post('/Home/DownloadStickers', { html: $("#page-for-printing").html() } , function (data) {
+    //    console.log(data);
+    //});
+    var paperSize = "";
+    if ($(".a4size").hasClass('active-paper')) {
+        paperSize = "A4"
+    }
+    if ($(".UsPaper").hasClass('active-paper')) {
+        paperSize = "UsPaper"
+    }
+    $.ajax({
+        cache: false,
+        url: '/Home/DownloadStickers',
+        data: JSON.stringify({ html: $("#page-for-printing").html(), pagesize: paperSize }),
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            if (!data) {
+                $("#wrapper").overhang({
+                    type: "error",
+                    message: "Whoops! You think you can skip steps, but actually you can't!",
+                    duration: 2,
+                    upper: true,
+                });
+                return;
+            }
+            console.log(data.fileGuid);
+            //    window.location = '/Home/Download?fileGuid=' + data.fileGuid;
+        }
+    })
+});
+$("#button-wrapper").on('click', "#send-pdf-button", function () {
+    $(this).attr("id", "send-message");
+    $(this).removeClass("btn-danger");
+    $(this).addClass("btn-warning");
+    $("#email-user-pdf").show(500);
+
+
+    $(this).html("<span class='glyphicon glyphicon-envelope'></span> Press again to sent!")
+});
+$("#button-wrapper").on('click', "#send-message", function () {
+    var testEmail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!testEmail.test($('#input-user-email-pdf').val())) {
+        $("#wrapper").overhang({
+            type: "warn",
+            message: "This email is not appropriate, can you check what is going on!",
+            duration: 2,
+            upper: true
+        });
+        return;
+    }
+    if (!ValidatePaperBeforeFinnish()) {
+        return;
+    }
+    if (!ValidateTemplateBeforeFinnish()) {
+        return;
+    }
+    $(this).removeClass("btn-warning");
+    $(this).addClass("btn-success btn-lg");
+    $("#email-user-pdf").hide(500);
+
+    $(this).html("<span class='glyphicon glyphicon-envelope'></span> Message is sent very successfully! <br /> Click to send another.")
+    $(this).attr("id", "send-pdf-button");
+    //   $(this).unbind("click").click();
+});
+$("#button-wrapper").on('click', "#send-pdf-button", function () {
+    console.log("click email with pdf");
 });
 
+// Validation on elements
+function ValidatePaperBeforeFinnish() {
+    if (!$(".a4size").hasClass('active-paper') && !$(".UsPaper").hasClass('active-paper')) {
+        $("#wrapper").overhang({
+            type: "warn",
+            message: "You have to choose paper size!",
+            duration: 2,
+            upper: true
+        });
+        return false;
+    }
+    return true;
+};
+function ValidateTemplateBeforeFinnish() {
+     if (!$(".template").hasClass('active-template')) {
+        $("#wrapper").overhang({
+            type: "warn",
+            message: "You have to choose template!",
+            duration: 2,
+            upper: true
+        });
+        return false;
+     }
+     return true;
+};
+// FUnctions for adding classes to active elements
 function AddImageToPlaceHolder(elem) {
 
     if ($(elem).hasClass('active-box')) {
@@ -121,9 +216,29 @@ function TemplateClick(elem) {
   .done(function (data) {
       $('#page-for-printing').html(data);
       var fourColumnHeight = $(".template-box-square  .panel-body").width() * 2;
-        $("#page-for-printing  .panel-body").height(fourColumnHeight);
+
+      var width = $('.placeholder .panel').offsetParent().width();
+      var parentWidth = $('#page-for-printing').width();
+      // console.log(width);
+      // console.log(parentWidth);
+
+      var height = $('.placeholder .panel').offsetParent().height();
+      var parentHeight = $('#page-for-printing > .placeholder').offsetParent().height();
+      console.log(height);
+      console.log(parentHeight);
+
+      var percentWidth = 100 * width / parentWidth;
+      var percentHeight = 100 * fourColumnHeight / parentHeight;
+
+      console.log(percentHeight);
+      $("#page-for-printing  .panel-body").height(fourColumnHeight);
+
+      $(".placeholder").css('width', percentWidth + '%');
+
+
   });
 };
+
 $(".papersizes .panel").click(function () {
     $(".papersizes .panel").removeClass("active-paper");
     $(this).addClass("active-paper");
